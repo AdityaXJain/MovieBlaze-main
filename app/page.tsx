f4f3048a1489e1edb89e605acad981aec43718d8
aditya jain <adityajain6366@gmail.com>
2025-04-12 01:19:30 +0530

    fix: adjust padding in main layout for improved responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { SearchBar } from "@/components/SearchBar";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black p-4 max-w-7xl mx-auto">
+    <main className="min-h-screen bg-black p-4 px-36">
       {/* <SearchBar /> */}
       <MovieGrid />
     </main>
